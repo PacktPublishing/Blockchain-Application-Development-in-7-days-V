55307cce4051422ef08545d1a9fd2bea73da3246
Will Button <will@willbutton.co>
2019-02-26 00:50:13 -0700

    Add support for node v10.15.0 and Solidity 0.5.0

--- test/gamingTest.js
+++ test/gamingTest.js
@@ -7,25 +7,27 @@ contract('Gaming', async (accounts) => {
 
   before(async () => {
     gaming = await Gaming.deployed()
-    const fundGame = await gaming.fundGame({from: owner, value: web3.toWei(10, 'ether')})
+    const fundGame = await gaming.fundGame({from: owner, value: web3.utils.toWei('10', 'ether')})
   })
 
   it('Should record player losses', async() => {
-    const initialBalance = await web3.eth.getBalance(player1).toNumber()
+    const initialBalance = await web3.eth.getBalance(player1)
+    const initialBalanceInEther = Number(web3.utils.fromWei(initialBalance, 'ether'))
     const gameRound = await gaming.winOrLose(10, true, {
       from: player1,
-      value: web3.toWei(1, 'ether')
+      value: web3.utils.toWei('1', 'ether')
     })
-    const postBalance = await web3.eth.getBalance(player1).toNumber()
+    const postBalance = await web3.eth.getBalance(player1)
+    const postBalanceInEther = Number(web3.utils.fromWei(postBalance, 'ether'))
     const playerStats = await gaming.players(player1)
     assert.equal(playerStats[1].toNumber(), 1, 'The player should have 1 loss')
-    assert.isAtLeast(initialBalance, postBalance + 10, 'Player account should have decreased by the amount of the wager')
+    assert.isAtLeast(initialBalanceInEther, postBalanceInEther + 1, 'Player account should have decreased by the amount of the wager')
   })
 
   it('Should record player wins', async() => {
     const gameRound = await gaming.winOrLose(10, false, {
       from: player1,
-      value: web3.toWei(1, 'ether')
+      value: web3.utils.toWei('1', 'ether')
     })
     const playerStats = await gaming.players(player1)
     assert.equal(playerStats[0].toNumber(), 1, 'The player should have 1 win')
